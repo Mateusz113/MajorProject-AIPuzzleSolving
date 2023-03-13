f50bf0a0e53d64b6c9cd1f15b0209644c306122c
Mateusz113 <mateusz.ch.113@gmail.com>
2023-03-13 03:14:20 +0000

    Added DefaultSettings.kt that is used to store the default settings for the application

---
+++ src/app/src/main/java/uk/ac/aber/dcs/cs39440/maze_solver/util/settings/DefaultSettings.kt
@@ -0,0 +1,7 @@
+package uk.ac.aber.dcs.cs39440.maze_solver.util.settings
+
+//Default setting for the maze size
+var mazeSize = Pair(20, 30)
+
+//Default setting used for the algorithm
+var algorithmChosen = "BFS"
