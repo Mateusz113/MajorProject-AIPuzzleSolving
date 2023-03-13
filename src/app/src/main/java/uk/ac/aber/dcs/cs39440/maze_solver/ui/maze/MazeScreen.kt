1b77092e237b2007c75587a5f7181bb328932cd7
Mateusz113 <mateusz.ch.113@gmail.com>
2023-03-13 03:30:54 +0000

    Added changes to match the currently implemented features -Added the text to display the current application settings -Updated the maze loading to use the MazeMap.kt -Added the functionality to start the pathfinding to "Start" button -Added functionality to reload maze from file to "Stop" button

--- src/app/src/main/java/uk/ac/aber/dcs/cs39440/maze_solver/ui/maze/MazeScreen.kt
+++ src/app/src/main/java/uk/ac/aber/dcs/cs39440/maze_solver/ui/maze/MazeScreen.kt
@@ -1,109 +1,160 @@
 package uk.ac.aber.dcs.cs39440.maze_solver.ui.maze
 
+import android.util.Log
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateMap
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import uk.ac.aber.dcs.cs39440.maze_solver.R
 import uk.ac.aber.dcs.cs39440.maze_solver.ui.components.TopLevelScaffold
 import uk.ac.aber.dcs.cs39440.maze_solver.ui.theme.Maze_solverTheme
-import java.lang.invoke.ConstantCallSite
-import kotlin.random.Random
+import uk.ac.aber.dcs.cs39440.maze_solver.ui.theme.typography
+import uk.ac.aber.dcs.cs39440.maze_solver.util.algorithms.bfs
+import uk.ac.aber.dcs.cs39440.maze_solver.util.maze_map.GetMazeMap
+import uk.ac.aber.dcs.cs39440.maze_solver.util.node.Node
+import uk.ac.aber.dcs.cs39440.maze_solver.util.settings.algorithmChosen
+import uk.ac.aber.dcs.cs39440.maze_solver.util.settings.mazeSize
 
+//Variables used to stop the constant file reading on navigation recompositions
+var wasFileRead = false
+var mazeMapRead: MutableMap<Pair<Int, Int>, Node> = mutableMapOf()
 
+/**
+ * Maze screen composable
+ * @param navController Navigation controller used to switch between screens
+ */
 @Composable
 fun MazeScreen(
     navController: NavController
 ) {
+    //State of the maze map representation
+    var mazeMap = remember { mutableStateMapOf<Pair<Int, Int>, Node>() }
+    //State used to start the pathfinding
+    var isPathfinding by remember { mutableStateOf(false) }
+    //State used to start the reload of maze from the file
+    var isLoading by remember { mutableStateOf(false) }
+    //Coroutine scope for multithreading
     val coroutineScope = rememberCoroutineScope()
-    var array: Array<Int> = generateNumberList(20, 30)
 
-//    LaunchedEffect(key1 = Unit) {
-//        coroutineScope.launch {
-//            array = generateNumberList(20, 30)
-//        }
-//    }
-    val itemModifierBlack = Modifier
-        .border(50.dp, Color.Black)
-        .wrapContentSize()
-    val itemModifierWhite = Modifier
-        .border(50.dp, Color.White)
-        .wrapContentSize()
+    //Checks if the file was read on this application launch and does so if not
+    if (!wasFileRead) {
+        GetMazeMap(
+            fileName = "maze20x30.txt",
+            mazeSize = mazeSize,
+            mazeMap = mazeMap
+        )
+        wasFileRead = true
+        mazeMapRead = mazeMap
+    } else {
+        //Reloads the maze from saved variable
+        mazeMap = mazeMapRead as SnapshotStateMap<Pair<Int, Int>, Node>
+    }
+
+    //Launches the BFS algorithm when the state is changed
+    if (isPathfinding) {
+        LaunchedEffect(key1 = Unit) {
+            coroutineScope.launch(Dispatchers.Default) {
+                bfs(
+                    mazeWidth = mazeSize.first,
+                    mazeHeight = mazeSize.second,
+                    mazeMap = mazeMap,
+                    startX = 0,
+                    startY = 1,
+                    endX = mazeSize.first,
+                    endY = mazeSize.second - 1,
+                )
+                isPathfinding = false
+            }
+        }
+    }
+
+    //Reloads the maze from file on state change
+    if (isLoading) {
+        GetMazeMap(
+            fileName = "maze20x30.txt",
+            mazeSize = mazeSize,
+            mazeMap = mazeMap
+        )
+        isLoading = false
+    }
+
+    //Wrapper function for all the items on the page
     TopLevelScaffold(navController)
     { innerPadding ->
         ConstraintLayout(modifier = Modifier.padding(innerPadding)) {
-            val (maze, startBtn, stopBtn) = createRefs()
 
-            //Maze vertical grid representation
-            LazyVerticalGrid(
-                columns = GridCells.Fixed(20),
+            //Refs for constraint layout definitions
+            val (settingsText, maze, startBtn, stopBtn) = createRefs()
+
+            //Displays the current chosen algorithm as well as maze size
+            AlgorithmSettingsText(
+                mazeSize = mazeSize,
+                algorithm = algorithmChosen,
                 modifier = Modifier
-                    .constrainAs(maze) {
+                    .fillMaxWidth()
+                    .constrainAs(settingsText) {
                         top.linkTo(parent.top)
                         start.linkTo(parent.start)
-                    },
-                userScrollEnabled = false
-            ) {
-                array.forEach { number ->
-                    item {
-                        if (number == 0) {
-                            Text(
-                                text = number.toString(),
-                                modifier = itemModifierBlack
-                            )
-                        } else {
-                            Text(
-                                text = number.toString(),
-                                modifier = itemModifierWhite
-                            )
-                        }
                     }
-                }
-            }
+            )
 
-            //Start button
+            //Maze vertical grid representation
+            RenderMaze(
+                mazeMap = mazeMap,
+                columnCount = GridCells.Fixed(mazeSize.first + 1),
+                modifier = Modifier
+                    .constrainAs(maze) {
+                        top.linkTo(settingsText.top)
+                    }
+                    .padding(top = 50.dp)
+                    .height(620.dp),
+                userScrollEnabled = false
+            )
+
+            //Start button - starts pathfinding onClick
             Button(
-                onClick = { array = generateNumberList(20, 30) },
+                onClick = {
+                    isPathfinding = true
+                    Log.i("BFS", mazeMap.entries.toString())
+                },
                 modifier = Modifier
                     .constrainAs(startBtn) {
                         start.linkTo(parent.start)
                         top.linkTo(maze.bottom)
                     }
-                    .padding(start = 50.dp, top = 30.dp)
+                    .padding(start = 50.dp, top = 20.dp)
                     .width(120.dp)
             ) {
                 Text(text = stringResource(R.string.start))
             }
 
-            //Stop button
+            //Stop button - reloads the maze onClick
             Button(
                 onClick = {
-                    coroutineScope.launch {
-                        array = generateNumberList(20, 30)
-                    }
+                    isLoading = true
                 },
                 modifier = Modifier
                     .constrainAs(stopBtn) {
                         end.linkTo(parent.end)
                         top.linkTo(maze.bottom)
                     }
-                    .padding(end = 50.dp, top = 30.dp)
+                    .padding(end = 50.dp, top = 20.dp)
                     .width(120.dp)
             ) {
                 Text(text = stringResource(R.string.stop))
@@ -112,14 +163,82 @@ fun MazeScreen(
     }
 }
 
-private fun generateNumberList(x: Int, y: Int): Array<Int> {
-    var gridArray = arrayOf<Int>()
-    for (i in (1..x * y)) {
-        val nextInt = Random.nextInt(0, 2)
-        gridArray += nextInt
+@Composable
+private fun RenderMaze(
+    mazeMap: MutableMap<Pair<Int, Int>, Node>,
+    columnCount: GridCells,
+    modifier: Modifier,
+    userScrollEnabled: Boolean
+) {
+    val itemModifierBlack = Modifier
+        .border(50.dp, Color.Black)
+        .height(20.dp)
+        .wrapContentSize()
+    val itemModifierWhite = Modifier
+        .border(50.dp, Color.White)
+        .height(20.dp)
+        .wrapContentSize()
+    val itemModifierRed = Modifier
+        .border(50.dp, Color.Red)
+        .height(20.dp)
+        .wrapContentSize()
+    val itemModifierBlue = Modifier
+        .border(50.dp, Color.Blue)
+        .height(20.dp)
+        .wrapContentSize()
+
+    LazyVerticalGrid(
+        columns = columnCount,
+        modifier = modifier,
+        userScrollEnabled = userScrollEnabled
+    ) {
+        if (mazeMap.isNotEmpty()) {
+            for (row in 0..mazeSize.second) {
+                for (column in 0..mazeSize.first) {
+                    item {
+                        if (mazeMap[Pair(column, row)] == Node.Wall) {
+                            Text(
+                                text = "",
+                                modifier = itemModifierBlack
+                            )
+                        } else if (mazeMap[Pair(column, row)] == Node.FinalPath) {
+                            Text(
+                                text = "",
+                                modifier = itemModifierRed
+                            )
+                        } else if (mazeMap[Pair(column, row)] == Node.Considered) {
+                            Text(
+                                text = "",
+                                modifier = itemModifierBlue
+                            )
+                        } else {
+                            Text(
+                                text = "",
+                                modifier = itemModifierWhite
+                            )
+                        }
+                    }
+                }
+            }
+        }
     }
-    return gridArray
 }
+
+
+@Composable
+private fun AlgorithmSettingsText(
+    mazeSize: Pair<Int, Int>,
+    algorithm: String,
+    modifier: Modifier
+) {
+    Text(
+        modifier = modifier.padding(top = 5.dp),
+        textAlign = TextAlign.Center,
+        style = TextStyle(fontSize = typography.titleLarge.fontSize),
+        text = "Chosen algorithm: ${algorithm}, Maze size: ${mazeSize.first}x${mazeSize.second}",
+    )
+}
+
 
 @Preview
 @Composable
