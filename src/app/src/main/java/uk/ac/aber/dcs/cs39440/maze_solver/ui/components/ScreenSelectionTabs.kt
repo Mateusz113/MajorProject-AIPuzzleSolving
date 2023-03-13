d178befa0cc68cab04c22615430fb6068b4e3a82
Mateusz113 <mateusz.ch.113@gmail.com>
2023-03-13 03:18:56 +0000

    Removed the unused import statements

--- src/app/src/main/java/uk/ac/aber/dcs/cs39440/maze_solver/ui/components/ScreenSelectionTabs.kt
+++ src/app/src/main/java/uk/ac/aber/dcs/cs39440/maze_solver/ui/components/ScreenSelectionTabs.kt
@@ -1,13 +1,10 @@
 package uk.ac.aber.dcs.cs39440.maze_solver.ui.components
 
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
